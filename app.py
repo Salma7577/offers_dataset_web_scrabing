d55593beae14332a6f5d8ad55e1b0c3aa38d6ea1
Salma7577 <salman757@outlook.sa>
2022-07-30 18:14:13 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,50 @@
+
+from base64 import encode
+import requests
+from bs4 import BeautifulSoup
+import csv
+from itertools import zip_longest
+
+
+link = "https://www.alsoouq.com/saudi-arabia-offers/panda-offers/page/"
+# print(result.content)
+
+weekly_offers_link = []
+for i in range(1, 3):
+    src = requests.get(link+str(i)).content
+    soup = BeautifulSoup(src, "lxml")
+    weekly_offers_link.extend(soup.select("a.more-link"))
+
+# print(weekly_offers_link[1].attrs["href"])
+
+for i in range(len(weekly_offers_link)):
+    weekly_offers_link[i] = weekly_offers_link[i].attrs["href"]
+
+
+# print(weekly_offers_link)
+
+
+src = requests.get(weekly_offers_link[1]).content
+soup = BeautifulSoup(src, "lxml")
+page_images = soup.select("noscript")
+print(page_images[1].text)
+# print([x.encode('utf-8') for x in soup.select("noscript")])
+
+
+# result = requests.get(
+#     "https://www.alsoouq.com/saudi-arabia-offers/panda-offers/page/1")
+# src = result.content
+# print(src)
+
+
+# print(soup)
+# weekly_offers_link = soup.find_all("a", {"class", "more-link"})
+# print(weekly_offers_link)
+
+
+# page_images = soup.find_all(
+#     "div", {"style": "width: 100%; text-align: center;"})
+# page_images = soup.find_all("noscript")
+
+
+# print([x.encode('utf-8') for x in page_images])
